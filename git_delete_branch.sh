504b7cf588a5bf7e10212097ea97d3c89df3729b
ATTY Lionel <yoyonel@hotmail.com>
2016-03-25 22:06:29 +0100

    Ajout des urls utilisés/inspirant les scripts

--- git_delete_branch.sh
+++ git_delete_branch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # usage: . git_delete_branch.sh <nom_de_la_branche>
 # Efface en local et remote une branche
+# url:
+# - http://stackoverflow.com/questions/2003505/delete-a-git-branch-both-locally-and-remotely
 git push origin --delete $1
 git branch -D $1
